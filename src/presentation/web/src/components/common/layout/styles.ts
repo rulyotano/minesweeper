01b1daae97c42c8e6d6edb02a03f15fa3c2973fa
Raul <rulyotano@gmail.com>
2023-08-28 14:16:56 +0200

    disable text selection

--- src/presentation/web/src/components/common/layout/styles.ts
+++ src/presentation/web/src/components/common/layout/styles.ts
@@ -3,6 +3,9 @@ import createStyles from "@material-ui/core/styles/createStyles";
 export default () =>
   createStyles({
     root: {
-      flexGrow: 1,
+      flexGrow: 1,      
+      "user-select": "none",
+      "-webkit-user-select": "none",
+      "-ms-user-select: none": "none"
     },
   });
